cc9dc421d4013cbb85403302a23416cbc8e5a0b1
fdgh134 <fdgh134@naver.com>
2024-06-10 14:20:19 +0900

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -129,6 +129,7 @@ $(function(){
   } else {
     welcomeSection.remove();
     $('.wrapper').remove();
+    $('body').css('overflow', 'auto');
     $('.profile').addClass("animate__lightSpeedInLeft");
     $('.profile, .introduce, .career, .btnframe, .content, .content .grid, .content .grid .grud__title, .card, .content2, .content2_inner, .contact').removeClass("none");
 
